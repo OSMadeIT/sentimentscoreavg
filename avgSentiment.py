b6ba437ec85fedab8a50ed7139704ec0b9337e1a
osmadeit <osmadeit@gmail.com>
2018-08-21 09:15:12 +0300

    pandas average and saving file

--- avgSentiment.py
+++ avgSentiment.py
@@ -1,27 +1,24 @@
-import pandas as pd
+import pandas as pd 
+import datetime
 
-#Open file of interest
-csv_file = pd.read_csv('avgsentiment/msft.csv')
-#Select columns of interest by their column name
-tiker, date, score = csv_file.Tiker, csv_file.Date, csv_file.Score
+# list of stocks
+# stockTickers = ['aapl', 'amzn', 'fb', 'gm', 'goog', 'googl', 'msft', 'nflx', 'tsla', 'twtr']
+# only use these 2 tickers to save on streaming time
+stockTickers = ['aapl', 'amzn']
 
-t_list, d_list, s_list = [], [], []
+def calculate_average(filename):
+    # read all stock files where average is to be calculated
+    df = pd.read_csv('Tweets CSV Files/#' + filename + '.csv')
+    df.columns = ['Ticker', 'id', 'date', 'tweet', 'polarity']
+    df['date']= df['date'].apply(pd.to_datetime)
+    df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    df2 = df.groupby('date')['polarity'].mean().apply(lambda x: '{:.4f}'.format(x))
+    df2.to_csv('Average Polarity CSVs/avg_'+filename+'.csv')
 
-for t in tiker:
-    t_list.append(t)
+    print(df2)
 
-for d in date:
-    d_list.append(d)
-
-for s in score:
-    s_list.append(s)
-
-print(len(s_list))
-#
-# new_data = {'Tiker': t_list,
-#             'Date': d_list,
-#             'Score': s_list}
-#
-# df = pd.DataFrame(new_data, columns = ['Tiker', 'Date', 'Score'])
-# df.to_csv('avgsentiment/new_data.csv')
-# print("New file has been saved")
+# looping through stock ticker list and saving tweets
+def calculateAvgPolarity():
+    for ticker in stockTickers:
+        calculate_average(ticker)
+print(ticker + ' saved')
